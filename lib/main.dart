6d24481ae9e4e6247f70e3f33d5499c607845161
Esad Dursun <3723esad@gmail.com>
2022-05-23 23:23:25 +0200

    web3dart package version changed. abi.json file created under assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +13,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Blockchain App',
-      home: Center(child: Text('Flutter Blockchain App'),),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  late Client httpClient;
+  late Web3Client ethClient;
+
+  final myAddress = '0x47452E2bb407986377E7F89B04AC087e7a0FF00a';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Flutter Blockchain App'),
     );
   }
 }
